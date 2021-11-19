3987d36e5afa0928242e3db1284c5304b19486ab
Hailx12 <87147375+Hailx12@users.noreply.github.com>
2021-11-19 21:10:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <a
+          href="https://github.com/Hailx12/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        , by Sohaily Kerpens
+      </footer>
+    </div>
+  );
 }
 
 export default App;
