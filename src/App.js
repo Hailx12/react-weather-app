0684cb3b9e483127344f3977bffc48a8cd180af6
Hailx12 <87147375+Hailx12@users.noreply.github.com>
2021-11-20 16:25:10 +0100

    Created weather component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        <a
-          href="https://github.com/Hailx12/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        , by Sohaily Kerpens
-      </footer>
+      <div className="Container">
+        Weather App
+        <Weather />
+        <footer>
+          <a
+            href="https://github.com/Hailx12/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          , by Sohaily Kerpens
+        </footer>
+      </div>
     </div>
   );
 }
