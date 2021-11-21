4481813d41c19de646462fe6e7020c860944d6a6
Hailx12 <87147375+Hailx12@users.noreply.github.com>
2021-11-21 17:51:57 +0100

    added fahrenheit element

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemp(props) {
-  return (
-    <span className="WeatherTemp">
-      <span className="current-temp">{Math.round(props.celsius)} </span>
-      <span className="temp-unit">°C</span>
-    </span>
-  );
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <span className="WeatherTemp">
+        <span className="current-temp">{Math.round(props.celsius)} </span>
+        <span className="temp-unit">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </span>
+    );
+  } else {
+    return (
+      <span className="WeatherTemp">
+        <span className="current-temp">{Math.round(fahrenheit())} </span>
+        <span className="temp-unit">
+          <a href="/" onClick={showCelsius}>
+            °C{" "}
+          </a>
+          | °F
+        </span>
+      </span>
+    );
+  }
 }
